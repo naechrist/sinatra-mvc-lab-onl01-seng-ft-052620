153b929adfa1d9cf522f46e2cac55d643bf921a1
Jenae Christensen <729jenae.christensen@gmail.com>
2020-07-21 18:00:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
-require_relative 'config/environment'
+require_relative '../config/environment.rb'
 
-class App < Sinatra::Base
-end
+
+class App < Sinatra::Base	class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @text = params[:user_phrase]
+    @piglatinizer = PigLatinizer.new
+    erb :piglatinize
+  end
+end 
